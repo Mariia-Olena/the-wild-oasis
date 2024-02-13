af159cef8fa1de7d257a952beda26b6603e85351
Mariia-Olena <mariia.stus@gmail.com>
2024-02-13 13:10:46 +0200

    implement routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,33 @@
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
+import Dashboard from './pages/Dashboard';
+import PageNotFound from './pages/PageNotFound';
+import Login from './pages/Login';
+import Account from './pages/Account';
+import Settings from './pages/Settings';
+import Users from './pages/Users';
+import Cabins from './pages/Cabins';
+import Bookings from './pages/Bookings';
+import GlobalStyles from './styles/GlobalStyles';
+
 function App() {
-  return <div>Hello world</div>;
+  return (
+    <>
+      <GlobalStyles />
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Navigate replace to='dashboard' />} />
+          <Route path='dashboard' element={<Dashboard />} />
+          <Route path='bookings' element={<Bookings />} />
+          <Route path='cabins' element={<Cabins />} />
+          <Route path='users' element={<Users />} />
+          <Route path='settings' element={<Settings />} />
+          <Route path='account' element={<Account />} />
+          <Route path='login' element={<Login />} />
+          <Route path='*' element={<PageNotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
